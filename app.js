1030dfbc46730c5669f43bf315ece459bf2f60fa
Eldorbek2001 <eldorbek2001@email.arizona.edu>
2022-05-24 17:07:54 -0700

    create ejs templates for different pages

--- app.js
+++ app.js
@@ -6,16 +6,27 @@ bodyParser = require("body-parser");
 app = express();
 app.use(bodyParser.urlencoded({extended : true}))
 app.use(express.static("public"));
-
+app.set('view engine', 'ejs');
 
 
 
 app.get("/", function(req, res){
+  res.render('list', {content: 'Main content'});
+});
 
-  res.sendFile(__dirname + "/index.html");
-
+app.get("/Newsletter", function(req, res){
+  res.render('list', {content: 'News letter content'});
 })
 
+app.get("/Events", function(req, res){
+  res.render('list', {content: 'Events content'});
+})
+app.get("/Alumnii", function(req, res){
+  res.render('list', {content: 'Alumnii content'});
+})
+app.get("/About", function(req, res){
+  res.render('list', {content: 'Main content'});
+})
 
 
 app.listen(3000, function(){
