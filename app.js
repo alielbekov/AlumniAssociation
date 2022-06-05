d7ffd3e58d04f6eadd06cf8efa47151c8e090612
Eldorbek2001 <eldorbek2001@email.arizona.edu>
2022-06-04 20:26:44 -0700

    add login and register bar, fix typos, fix news adding page

--- app.js
+++ app.js
@@ -13,68 +13,76 @@ app.set('view engine', 'ejs');
 
 var newsSchema = {
 author: String,
-titlle: String,
+title: String,
 date: String,
 content: String
 }
+
 var alumniSchema = {
 fname: String,
 lname: String,
 mail: String,
-photoPath: String
+photoPath: String,
+role: String
 }
 
 const News = mongoose.model("news", newsSchema);
 const Alumni = mongoose.model("users", alumniSchema);
-var alumniObj = Alumni.find(function(err, callback){if(err){console.log(err)}});
-var newsObj = News.find(function(err, callback){if(err){console.log(err)}});
-var alumniHtml = alumniObject.getContentHtml(alumniObj);
-var newslettersHtml = newsletterObject.getContentHtml(newsObj);
+
+
+//
+// var alumniHtml = alumniObject.getContentHtml(alumniObj);
+// var newslettersHtml = newsletterObject.getContentHtml(newsObj);
 
 
 app.get("/", function(req, res) {
   res.render('list', {content: 'Main content'});
 });
 
-app.get("/Newsletter", function(req, res) {
-  res.render('list', {
-    content: newslettersHtml
-  });
+app.get("/News", function(req, res) {
+  var newsObj = News.find(function(err, callback){
+    if(err){ res.render('list', {content: err});
+    }else{
+      const newslettersHtml = newsletterObject.getContentHtml(callback);
+      res.render('list', {content: newslettersHtml});
+    }});
+
 })
 
-app.get("/Newsletter/add", function(req, res) {
+app.get("/News/add", function(req, res) {
   res.render('addNewsletter', {});
 })
 
-app.post("/", function(req, res) {
-  var newsletterAuthor = req.body['newsletter-author']
-  var newsletterTitle = req.body['newsletter-title']
-  var newsletterContent = req.body['newsletter-content']
-
-  newsletters.unshift({author: newsletterAuthor,
-                       title: newsletterTitle,
-                       date: "Date here",
-                       content: newsletterContent});
-  // Need to think about the below line
-  newslettersHtml  =  newsletterObject.getContentHtml(newsletters);
-  res.redirect("/Newsletter");
+app.post("/News", function(req, res) {
+  var newsletterAuthor = req.body['newsletter-author'];
+  var newsletterTitle = req.body['newsletter-title'];
+  var newsletterContent = req.body['newsletter-content'];
+  const news = new News({
+    author: newsletterAuthor,
+    title: newsletterTitle,
+    date: "Today",
+    content: newsletterContent
+  });
+  news.save();
+  res.redirect("/News");
 })
 
 app.get("/Events", function(req, res) {
-  res.render('list', {
-    content: 'Events content'
-  });
+  res.render('list', {content: 'Events content'});
 })
 
 app.get("/Alumnii", function(req, res) {
-  res.render('list', {
-    content: alumniHtml
-  });
+  var alumniObj = Alumni.find(function(err, callback){
+    if(err){res.render('list', {content: err});
+    }else{
+      const alumniHtml = alumniObject.getContentHtml(callback);
+      res.render('list', {content: alumniHtml});
+    }});
 })
 
-app.get("/About", function(req, res) {
+app.get("/faqs", function(req, res) {
   res.render('list', {
-    content: 'Main content'
+    content: 'FAQs'
   });
 })
 
--- views/newsletterContent.js
+++ views/newsletterContent.js
@@ -5,7 +5,7 @@ module.exports.getContentHtml  = function(newsletters){
     retString +='<div class="container">';
     retString +='<div class="row justify-content-center">';
     retString +='<div class="col-4 news-letter-title">';
-    retString +='<h1>'+newsletters[i].title+'</h1></div></div>';
+    retString +='<h1>' + newsletters[i].title + '</h1></div></div>';
     retString += '<div class="row">';
     retString += '<div class="col-12 newsletter-content">';
     retString +=  newsletters[i].content + "</div></div>";
