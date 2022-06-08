10ada353d123ce611bdcc125d829cb6522f1dadc
Eldorbek2001 <eldorbek2001@email.arizona.edu>
2022-06-08 01:08:24 -0700

    fix some css, make changes on remote db, create gitignore and env files

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // JShint esversion: 6
-
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const mongoose = require("mongoose");
 const alumniObject = require(__dirname + "/views/alumniContent.js");
 const newsletterObject = require(__dirname + "/views/newsletterContent.js");
-mongoose.connect("mongodb://localhost:27017/test", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://"+process.env.API_USERNAME+":"+process.env.API_KEY+"@cluster0.mjlk1.mongodb.net/?retryWrites=true&w=majority/testDB/test", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -14,9 +14,11 @@ app.set('view engine', 'ejs');
 var newsSchema = {
 author: String,
 title: String,
-date: String,
-content: String
+date: Object,
+content: String,
+source: String,
 }
+
 var alumniSchema = {
 fname: String,
 lname: String,
@@ -28,12 +30,13 @@ const News = mongoose.model("news", newsSchema);
 const Alumni = mongoose.model("users", alumniSchema);
 
 
+
 app.get("/", function(req, res) {
   res.render('list', {content: 'Main content'});
 });
 
 app.get("/News", function(req, res) {
-  var newsObj = News.find(function(err, callback){
+  var newsObj = News.find({}).sort([['date', -1]]).exec(function(err, callback){
     if(err){ res.render('list', {content: err});
     }else{
       const newslettersHtml = newsletterObject.getContentHtml(callback);
@@ -47,13 +50,16 @@ app.get("/News/add", function(req, res) {
 })
 
 app.post("/News", function(req, res) {
+
+  const time = new Date();
   var newsletterAuthor = req.body['newsletter-author'];
   var newsletterTitle = req.body['newsletter-title'];
   var newsletterContent = req.body['newsletter-content'];
+
   const news = new News({
     author: newsletterAuthor,
     title: newsletterTitle,
-    date: "Today",
+    date: time,
     content: newsletterContent
   });
   news.save();
