d0b1c8ea7b5b7d54b97f56ca40ab0f6934c4e39e
Eldorbek2001 <eldorbek2001@email.arizona.edu>
2022-09-11 15:43:04 -0700

    setup telegram bot

--- app.js
+++ app.js
@@ -7,36 +7,100 @@ const app = express();
 const mongoose = require("mongoose");
 const alumniObject = require(__dirname + "/views/alumniContent.js");
 const newsletterObject = require(__dirname + "/views/newsletterContent.js");
-mongoose.connect("mongodb+srv://"+process.env.API_USERNAME+":"+process.env.API_KEY+"@cluster0.mjlk1.mongodb.net/?retryWrites=true&w=majority/testDB/test", {useNewUrlParser: true});
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
-app.set('view engine', 'ejs');
-
-
-
-var newsSchema = {
+mongoose.connect("mongodb+srv://"+process.env.API_USERNAME+":"+process.env.API_KEY+"@cluster0.mjlk1.mongodb.net/?retryWrites=true&w=majority/testDB/test", { useNewUrlParser: true, useUnifiedTopology: true, writeConcern: { w: 'majority', j: true, wtimeout: 1000 }})
+const newsSchema = new mongoose.Schema({
 author: String,
 title: String,
 date: Object,
 content: String,
 source: String,
-}
+});
 
-var alumniSchema = {
+const alumniSchema = new mongoose.Schema({
 fname: String,
 lname: String,
 mail: String,
 photoPath: String,
 role: String
-}
-var authSchema = {
+});
+
+const authSchema = new mongoose.Schema({
   email: String,
   pwd: String
-}
+});
+
+
 const User = mongoose.model("auth", authSchema);
 const News = mongoose.model("news", newsSchema);
 const Alumni = mongoose.model("users", alumniSchema);
 
+
+
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+const {Telegraf} = require("telegraf");
+const bot = new Telegraf(process.env.BOT_API);
+var title = false;
+var content = false;
+
+var heading = ""
+var message = ""
+var author = ""
+
+bot.command("post", (ctx)=>{
+  title = true;
+  ctx.reply("Enter title:")
+  author = ctx.message.from.first_name;
+})
+bot.command("clear", (ctx)=>{
+  title = false;
+  content = false;
+  heading = ""
+  message = ""
+  author = ""
+})
+
+bot.on("message", (ctx)=>{
+  if(title){
+    heading = ctx.message.text;
+    ctx.reply("Your title is '"+ heading + "'.")
+    ctx.reply("Send your content  ")
+    title = false
+    content = true;
+  }
+  else if(content){
+    message = ctx.message.text;
+    ctx.reply("Your content is '"+ message + "'.")
+    content = false;
+    ctx.reply("Your post should be up very soon!");
+    addData(author, heading, message);
+  }
+})
+bot.launch();
+
+function addData(author, heading, message){
+  const time = new Date();
+  var newsletterAuthor = author;
+  var newsletterTitle = heading;
+  var newsletterContent = message;
+  const news = new News({
+      author: newsletterAuthor,
+      title: newsletterTitle,
+      date: time,
+      content: newsletterContent,
+      source: "Telegram"
+    });
+  news.save();
+  title = false;
+  content = false;
+  heading = ""
+  message = ""
+  author = ""
+}
 
 app.get("/", function(req, res) {
   res.render('list', {content: 'home', htmls:""});
@@ -53,9 +117,41 @@ app.get("/news", function(req, res) {
 
 })
 
+app.get("/alumnii", function(req, res) {
+  var alumniObj = Alumni.find(function(err, callback){
+    if(err){res.render('list', {content: err});
+    }else{
+      const alumniHtml = alumniObject.getContentHtml(callback);
+      //fix data !!!
+      res.render('list', {content:"alumni", htmls:alumniHtml});
+    }});
+})
+
+app.get("/login", function(req, res) {
+  res.render('list', {content: 'login',
+htmls:""});
+});
+
+app.get("/register", function(req, res) {
+  res.render('list', {content: 'register',
+htmls:""});
+});
+
+app.get("/faqs", function(req, res) {
+  res.render('list', {content: 'faqs',
+htmls:""});
+})
+
+app.get("/events", function(req, res) {
+  res.render('list', {content: 'events',
+htmls:""});
+})
+app.get("/addNews", function(req, res) {
+  res.render('list', {content: 'addNewsLetter',
+htmls:""})})
+
 
 app.post("/news", function(req, res) {
-
   const time = new Date();
   var newsletterAuthor = req.body['newsletter-author'];
   var newsletterTitle = req.body['newsletter-title'];
@@ -69,36 +165,6 @@ app.post("/news", function(req, res) {
   });
   news.save();
   res.redirect("/news");
-})
-
-app.get("/alumnii", function(req, res) {
-  var alumniObj = Alumni.find(function(err, callback){
-    if(err){res.render('list', {content: err});
-    }else{
-      const alumniHtml = alumniObject.getContentHtml(callback);
-      //fix data !!!
-      res.render('list', {content:"alumni", htmls:alumniHtml});
-    }});
-})
-
-app.get("/login", function(req, res) {
-  res.render('list', {content: 'login',
-htmls:"a"});
-});
-
-app.get("/register", function(req, res) {
-  res.render('list', {content: 'register',
-htmls:"a"});
-});
-
-app.get("/faqs", function(req, res) {
-  res.render('list', {content: 'faqs',
-htmls:"a"});
-})
-
-app.get("/events", function(req, res) {
-  res.render('list', {content: 'events',
-htmls:"a"});
 })
 
 
